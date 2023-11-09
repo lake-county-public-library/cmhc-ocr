adb679c3ac88bcec66d571dd7fe1dc3aae5ab218
awoods <awoods01@gmail.com>
2023-11-09 00:44:14 +0000

    Update Docker to be daemon

--- bin/1-start-container.sh
+++ bin/1-start-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm --name aw-ocr1 --mount type=bind,source=/data/programming/,target=/app -it aw-ocr:latest
+docker run -d --rm --name aw-ocr1 --mount type=bind,source=/data/programming/,target=/app -it aw-ocr:latest
 
